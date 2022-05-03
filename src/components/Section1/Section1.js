fcc69fb25f477356ee70473068939c0d304e6982
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-03 13:10:43 +0430

    sec2

--- src/components/Section1/Section1.js
+++ src/components/Section1/Section1.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Section1.css";
 
+
 const Section1 = () => {
   return (
     <span className="sec1">
-      <h1 className="text1">Hi, I am Brooke, a project manager</h1>
-      <img src="/img/img1.avif" className="img1" />
+      <div className="div1">
+        <h1 className="text1">Hi, I am Brooke, a project manager</h1>
+        <button className="btn1">Email Me</button>
+      </div>
+      <div className="div2">
+        <img src="/img/img1.avif" className="img1" />
+      </div>
     </span>
   );
 };
