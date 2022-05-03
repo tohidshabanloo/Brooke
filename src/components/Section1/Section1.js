90ada2ec9ce85f9740d8014023196d9be1d7d1d2
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-03 23:00:16 +0430

    header + sec1 completed

--- src/components/Section1/Section1.js
+++ src/components/Section1/Section1.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./Section1.css";
 
-
 const Section1 = () => {
   return (
     <span className="sec1">
       <div className="div1">
         <h1 className="text1">Hi, I am Brooke, a project manager</h1>
         <button className="btn1">Email Me</button>
+        <div className="links">
+          <a href="https://www.linkdeen.com">linkdeen</a> /
+          <a href="https://www.facebook.com"> Facebook</a> /
+          <a href="https://www.twitter.com">Twitter</a>
+        </div>
       </div>
+
       <div className="div2">
         <img src="/img/img1.avif" className="img1" />
       </div>
