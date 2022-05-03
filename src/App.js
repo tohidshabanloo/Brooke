aee79d10ed7e58065c02752810ef9e807ac2edf7
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-03 23:53:17 +0430

    font added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import Section1 from "./components/Section1/Section1.js";
+import Section2 from "./components/Section2/Section2.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Section1 />
+      <Section2 />
     </div>
   );
 }
--- src/components/Section2/Section2.js
+++ src/components/Section2/Section2.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./Section2.css";
 
 const Section2 = () => {
-  return <div className="sec2">Section2</div>;
+  return (
+    <div className="container2">
+      <div className="text1">
+        <h6>― My background</h6>
+        <h6>
+          I’ve gone through a number of phases in my career, and each of them
+          has informed my current work ethos and philosophy. I have excelled in
+          a number of project management roles in diverse industries, and
+          believe that my number one asset is my ability to adapt to any
+          challenge that is thrown my way.
+        </h6>
+      </div>
+      <div className="list1">
+        ― Skills
+        <ul>
+          <li>Leadership</li>
+          <li>Communication </li>
+          <li>Critical thinking</li>
+          <li>Risk management</li>
+          <li>Cost management</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Section2;
