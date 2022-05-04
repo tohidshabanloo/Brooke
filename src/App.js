1e567c542fb6bc284a3ecc8ec3af2aff8b826922
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-04 14:26:08 +0430

    section 4 completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header.js";
 import Section1 from "./components/Section1/Section1.js";
 import Section2 from "./components/Section2/Section2.js";
 import Section3 from "./components/Section3/Section3.js";
+import Section4 from "./components/Section4/Section4";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
---
+++ src/components/Section4/Section4.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Section4.css";
+
+const Section4 = () => {
+  return (
+    <div className="container4">
+      <div className="matn1">
+        <div className="title1">
+          <h3>― Work experience</h3>
+        </div>
+        <div className="list-container">
+          <div className="list2">
+            <h2 className="title2">Senior project manager</h2>
+            <h3 className="title3">Stilltech / Aug 2018 - Present</h3>
+            <h3>
+              I’ve been central to delivering two extensive projects while
+              working at Stilltech, both in the augmented reality space.
+            </h3>
+          </div>
+          <div className="list2">
+            <h2 className="title2">Project manager</h2>
+            <h3 className="title3">Yeall & Co / Aug 2016 - Aug 2018</h3>
+            <h3>
+              I was lucky to learn the ropes at Yeall Co., where I worked with
+              an international team on projects including edible smartphone
+              cases and human cloning.
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className="matn2">
+        <div className="title">
+          <h3>― Education</h3>
+        </div>
+        <div className="list-container2">
+          <div className="list2">
+            <h2 className="title2">Columbia University</h2>
+            <h3 className="title3">
+              Master's degree, International Business / 2016 - 2018
+            </h3>
+          </div>
+          <div className="list2">
+            <h2 className="title2">Stanford University</h2>
+            <h3 className="title3">
+              Bachelor’s degree, Business Administration / 2012 - 2016
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className="matn2">
+        <div className="title">
+          <h3>― Courses</h3>
+        </div>
+        <div className="list-container2">
+          <div className="list2">
+            <h2 className="title2">Agile methodology</h2>
+            <h3 className="title3">Spaetzle Institute / 2016</h3>
+          </div>
+          <div className="list2">
+            <h2 className="title2">Project management methodologies</h2>
+            <h3 className="title3">Knödl Labs / 2017</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section4;
