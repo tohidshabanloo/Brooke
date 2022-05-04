f75f80d2b9c8e02ec7dd12e5c9869c371e170b30
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-04 17:48:29 +0430

    section 5 completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,18 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="container">
-      <span className="span">
-        <p className="logo">BROOKE</p>
-        <div className="btn">
-          <button className="button">ABOUT ME</button>
-          <button className="button">EXPERIENCE</button>
-          <button className="button">TESTIMONIALS</button>
-          <button className="button">CONTACTS</button>
-        </div>
-      </span>
+    <div className="sticky">
+      <div className="container">
+        <span className="span">
+          <p className="logo">BROOKE</p>
+          <div className="btn">
+            <button className="button">ABOUT ME</button>
+            <button className="button">EXPERIENCE</button>
+            <button className="button">TESTIMONIALS</button>
+            <button className="button">CONTACTS</button>
+          </div>
+        </span>
+      </div>
     </div>
   );
 };
---
+++ src/components/Section5/Section5.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Section5.css";
+
+const Section5 = () => {
+  return (
+    <div className="container5">
+      <div className="text3">
+        <h1 className="matn4">
+          “Whenever I am in a difficult situation where there seems to be no way
+          out, I think about all the times I have been in such situations and
+          say to myself: I did it before, so I can do it again.”
+        </h1>
+        <h3 className="author">- Idowu Koyenikan</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Section5;
