3e82f3d43bce13506b9f6769b87136e283fe531a
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-04 13:23:36 +0430

    section3 completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header.js";
 import Section1 from "./components/Section1/Section1.js";
 import Section2 from "./components/Section2/Section2.js";
+import Section3 from "./components/Section3/Section3.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
---
+++ src/components/Section3/Section3.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Section3.css";
+
+const Section3 = () => {
+  return (
+    <div className="container3">
+      <img src="/img/img4.avif" className="img4" />
+      <img src="/img/img3.webp" className="img3" />
+      <img src="/img/img2.avif" className="img2" />
+    </div>
+  );
+};
+
+export default Section3;
