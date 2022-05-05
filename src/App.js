d1cb264f0ae88fae2f33ed594544d560d22cfc8c
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-05 12:33:05 +0430

    section8 completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header/Header.js";
-import Section1 from "./components/Section1/Section1.js";
-import Section2 from "./components/Section2/Section2.js";
-import Section3 from "./components/Section3/Section3.js";
+import Header from "./components/Header/Header";
+import Section1 from "./components/Section1/Section1";
+import Section2 from "./components/Section2/Section2";
+import Section3 from "./components/Section3/Section3";
 import Section4 from "./components/Section4/Section4";
 import Section5 from "./components/Section5/Section5";
 import Section6 from "./components/Section6/Section6";
 import Section7 from "./components/Section7/Section7";
+import Section8 from "./components/Section8/Section8";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Section5 />
       <Section6 />
       <Section7 />
+      <Section8 />
     </div>
   );
 }
---
+++ src/components/Section8/Section8.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Section8.css";
+
+const Section8 = () => {
+  return (
+    <div className="container8">
+      <div className="leftt">
+        <h1 className="brooke">Brooke R.</h1>
+        <h1 className="power">Powered by Zyro.com</h1>
+      </div>
+
+      <div className="middlee">― SOCIALS Linkedin Facebook Twitter</div>
+      <div className="rightt">― CONTACTS your@mail.com 123-123-1234</div>
+    </div>
+  );
+};
+
+export default Section8;
